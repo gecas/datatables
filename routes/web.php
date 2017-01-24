5b3b8d84f1d9c9205ee09f60379f94b8548f9e45
Marius Gečiauskas <geciauskasmarius@gmail.com>
2017-01-24 22:24:10 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::controller('datatables', 'DatatablesController', [
+//     'anyData'  => 'datatables.data',
+//     'getIndex' => 'datatables',
+// ]);
+
+Route::get('/datatables', 'DatatablesController@getIndex');
+
+Route::get('/datatables/data', 'DatatablesController@data');
